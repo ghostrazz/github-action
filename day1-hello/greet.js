7db75d8d05cef17337131812e89bf1a0b354710f
sakul sapkota <raxzsapkota@gmail.com>
2026-07-10 18:05:16 +0545

    New comment

--- day1-hello/greet.js
+++ day1-hello/greet.js
@@ -1,4 +1,6 @@
 const who = process.env.GREET_NAME || "world";
 const runNumber = process.env.GITHUB_RUN_NUMBER || "local";
 
+
+// extra comments to show that this is a new version of the file
 console.log(`Hello, ${who}! This is run #${runNumber}.`);
